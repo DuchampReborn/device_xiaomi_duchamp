01aeb2e41c7d55bf2ec1ce7838903dfdebe3f8aa
snapboss <mbajrami95@gmail.com>
2026-09-19 23:12:36 +0200

    duchamp: Inherit engineering mode

--- device.mk
+++ device.mk
@@ -143,6 +143,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     $(LOCAL_PATH)/configs/linker.config.json
 
+# Engineer Mode
+$(call inherit-product-if-exists, device/xiaomi/duchamp-engineering/device.mk)
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
