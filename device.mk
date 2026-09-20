a8e66f11a2f6b017d3458be068cf32110021a1d7
snapboss <mbajrami95@gmail.com>
2026-09-20 07:26:51 +0200

    duchamp: Silence SurfaceFlinger log spams
    It keeps spamming constantly 'surfaceflinger: Failed to get metadata. Mapper error: 7'. Let's silence it for now

--- device.mk
+++ device.mk
@@ -211,6 +211,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Logtag
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.log.tag.surfaceflinger=S
+
 # Media
 ifeq ($(TARGET_INCLUDES_DOLBY),true)
 PRODUCT_COPY_FILES += \
